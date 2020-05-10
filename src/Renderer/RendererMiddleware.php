8e9d7a784bdab80dd2681e4f2bf2b1256e32a233
Didier Moindreau <dmoindreau@gmail.com>
2020-05-10 10:16:00 +0200

    Add process method

--- src/Renderer/RendererMiddleware.php
+++ src/Renderer/RendererMiddleware.php
@@ -4,7 +4,27 @@
 namespace Renderer;
 
 
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
 class RendererMiddleware implements  MiddlewareInterface
 {
+    /**
+     * @param ServerRequestInterface $request
+     * @param RequestHandlerInterface $handler -
+     * @return ResponseInterface
+     */
+    function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+
+        $response = $handler->handle($request);
+        $render = $request->getAttribute("render");
+        if ($render) {
+            $response->getBody()->write($this->page);
+        }
+        return $response;
+    }
+
 
 }
